3abde2c21a1dc957df0678fcc4fe9c6a360badf1
JulSarmiento <jasa1999@hotmail.com>
2023-02-09 15:34:36 -0500

    connexion a la base de datos funcionado, pendiente descubrir porque no me funcionana las categorias

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ require('dotenv').config();
 const axios = require('axios');
 const { API_KEY } = process.env;
 
-// const URL = `https://api.steampowered.com/ISteamApps/GetAppList/v2/?lkey=${API_KEY}&count=20`;
-const URL = `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json`
+const getGamesByCategory = async (category) => {
+  try {
+    const URL = `https://api.rawg.io/api/games?key=${API_KEY}&genre=${category}`;
+    
+    const response =  await axios.get(URL, {
+      'headers': {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
 
-axios.get(URL)
-  .then(response => {
-    // const {apps} = response.data.applist;
-    // const result = apps.map(app => {
-    //   return {
-    //     id: app.appid,
-    //     name: app.name
-    //   }
-    // }
-    // );
-    // console.table(result);
-    console.log(response.data.appnews.newsitems)
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    return response.data.results.map((game) => {
+      return {
+        'name': game.name,
+        'genres': game.genres.map((genre) => genre.name)
+      }
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getGamesByCategory('Action')
+  .then((games) => console.log(games))
+  .catch((error) => console.log(error));
+    
