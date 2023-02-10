54f9013e5d4caeff479589c85e678e4290d3135d
JulSarmiento <jasa1999@hotmail.com>
2023-02-10 15:54:33 -0500

    challenge terminado

--- index.js
+++ index.js
@@ -2,79 +2,28 @@ require('dotenv').config();
 const axios = require('axios');
 const { API_KEY } = process.env;
 
-// const getGamesByCategory = async (category) => {
-//   try {
-//     const URL = `https://api.rawg.io/api/games?key=${API_KEY}&genre=${category}`;
-    
-//     const response =  await axios.get(URL, {
-//       'headers': {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//       }
-//     })
-
-//     return response.data.results.map((game) => {
-//       return {
-//         'name': game.name,
-//         'genres': game.genres.map((genre) => genre.name)
-//       }
-//     })
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getGamesByCategory('Action')
-//   .then((games) => console.log(games))
-//   .catch((error) => console.log(error));
-    
-
-// const instance = axios.create({
-//   baseURL: 'https://api.rawg.io/api/games'
-// });
-
-// async function getGamesByCategory(category) {
-//   try {
-//     const response = await instance.get('', {
-//       params: {
-//         genres: category,
-//         key: API_KEY
-//       }
-//     });
-//     const games = response.data.results.map((game) => {
-//       return {
-//         'name': game.name,
-//         'genres': game.genres.map((genre) => genre.name)
-//         };
-//       });
-//     console.log(games);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
 const instance = axios.create({
   baseURL: 'https://www.giantbomb.com/api/games/'
 });
 
-async function getGamesByCategory(category) {
+async function getGamesByCategory(plataform) {
   try {
     const response = await instance.get('', {
       params: {
-        filter: `genre:genre:${category}`,
+        filter: `plataform:plataform:${plataform}`,
+        offset: 100,
         format: 'json',
         limit: 10,
         api_key: API_KEY,
       }
     });
 
-    // console.log('response', response.data.results.length);
     const gamesArray = response.data.results
 
     const gamesToShow = gamesArray.map((game) => {
       return {
         'name': game.name,
-        'genres': game.genres ? game.genres.map((genre) => genre.name) : []
+        'platforms': game.platforms.map((platform) => platform.name)
       }
     })
 
@@ -85,5 +34,5 @@ async function getGamesByCategory(category) {
   }
 }
 
-getGamesByCategory('MMORPG');
+getGamesByCategory('snes');
 
